b82274142254136bd19da485fbf0f8e8e83fe3cc
ArianaEsperanza19 <uribeatencio2021@gmail.com>
2025-01-24 18:52:52 -0400

    Establecido atajo para ver info de un elemento

--- lua/atajos/atajos_debug.lua
+++ lua/atajos/atajos_debug.lua
@@ -1,23 +1,22 @@
 local M = {}
 
 M.show_keymaps = function()
-  local keymaps = [[
+	local keymaps = [[
   Go to definition:
   <leader>zd: :lua require("telescope.builtin").lsp_definitions()<CR>
   Go to references:
   <leader>zx: :lua require("telescope.builtin").lsp_references()<CR>
-
+  Show info about element:
+  <leader>zh: :lua vim.lsp.buf.hover()<CR> || enter to close
   ]]
 
-  local Terminal  = require('toggleterm.terminal').Terminal
-  local keymap_term = Terminal:new({
-    cmd = "echo \""..keymaps.."\" | less",
-    direction = "float",
-    hidden = true,
-  })
-  keymap_term:toggle()
+	local Terminal = require("toggleterm.terminal").Terminal
+	local keymap_term = Terminal:new({
+		cmd = 'echo "' .. keymaps .. '" | less',
+		direction = "float",
+		hidden = true,
+	})
+	keymap_term:toggle()
 end
 
 return M
-
-
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,24 +1,36 @@
-local dap = require('dap')
+local dap = require("dap")
 
 dap.configurations.php = {
-  {
-    type = 'php',
-    request = 'launch',
-    name = 'Launch file',
-    program = '${file}',
-    cwd = '${workspaceFolder}',
-    port = 9000,
-    stopOnEntry = true,
-  },
+	{
+		type = "php",
+		request = "launch",
+		name = "Launch file",
+		program = "${file}",
+		cwd = "${workspaceFolder}",
+		port = 9000,
+		stopOnEntry = true,
+	},
 }
 
-require('dap').adapters.php = {
-  type = 'executable',
-  command = 'node',
-  args = {"php-debug-adapter"},
+require("dap").adapters.php = {
+	type = "executable",
+	command = "node",
+	args = { "php-debug-adapter" },
 }
 
 -- Ver definicion
-vim.api.nvim_set_keymap('n', '<leader>zd', ':lua require("telescope.builtin").lsp_definitions()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>zd",
+	':lua require("telescope.builtin").lsp_definitions()<CR>',
+	{ noremap = true, silent = true }
+)
 -- Ver instancias
-vim.api.nvim_set_keymap('n', '<leader>zx', ':lua require("telescope.builtin").lsp_references()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>zx",
+	':lua require("telescope.builtin").lsp_references()<CR>',
+	{ noremap = true, silent = true }
+)
+-- Ver información de la función (hover)
+vim.api.nvim_set_keymap("n", "<leader>zh", ":lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true })
