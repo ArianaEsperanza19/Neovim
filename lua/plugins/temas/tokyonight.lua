6de9b708f4e4217305c9286b52b11d2c58741a88
Ariana <ArianaEsperanza19>
2025-05-17 19:06:26 -0400

    Juste a Tokyonight para atenuar ventanas inactivas

--- lua/plugins/temas/tokyonight.lua
+++ lua/plugins/temas/tokyonight.lua
@@ -3,6 +3,8 @@ return {
 	config = function()
 		require("tokyonight").setup({
 			style = "moon", -- "storm", "day", "moon" o "night"
+			dim_inactive = true, -- dims inactive windows
+			lualine_bold = true, -- When `true`, section headers in the lualine theme
 		})
 		-- Colocar en init
 		-- vim.cmd([[colorscheme tokyonight]])
