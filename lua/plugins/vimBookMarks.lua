dec3a17a31cc57480a86543a43c7d6f360f9610e
Ariana <ArianaEsperanza19>
2025-05-19 15:17:56 -0400

    Ajustes a vimBookmarks

--- lua/plugins/vimBookMarks.lua
+++ lua/plugins/vimBookMarks.lua
@@ -4,9 +4,21 @@ return {
 	"MattesGroeger/vim-bookmarks", -- Plugin de bookmarks
 	config = function()
 		-- Configuración personalizada para vim-bookmarks
-		vim.g.bookmark_sign = "🔖"
-		vim.g.bookmark_annotation_sign = "📝"
+		vim.g.bookmark_sign = "󰓹"
+		vim.g.bookmark_annotation_sign = "󰓽"
 		vim.g.bookmark_auto_save = 1
 		vim.g.bookmark_auto_close = 1
+		vim.g.bookmark_sign_priority = 10
+		-- Obtener el color del grupo de resaltado actual
+		local function get_hl_color(group, attr)
+			local hl = vim.api.nvim_get_hl_by_name(group, true)
+			return string.format("#%06x", hl[attr] or 0xFFFFFF) -- Color por defecto blanco si no se encuentra
+		end
+
+		-- Aplicar los colores del esquema actual a los bookmarks
+		vim.cmd(string.format("highlight BookmarkSign guifg=%s gui=bold", get_hl_color("WarningMsg", "foreground")))
+		vim.cmd(
+			string.format("highlight BookmarkAnnotationSign guifg=%s gui=bold", get_hl_color("String", "foreground"))
+		)
 	end,
 }
